574bce90a4e11a584b24afe1407acbae0a062957
Rushan Sekaev <ozdan@inbox.ru>
2013-12-03 14:20:34 +0400

    Added 2 app to installed apps

--- site_test_gallery/settings.py
+++ site_test_gallery/settings.py
@@ -123,6 +123,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django_extensions',
+    'sorl.thumbnail',
 )
 
 # A sample logging configuration. The only tangible logging
